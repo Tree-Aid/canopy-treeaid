91ec5eba4faaa3d42011caaad4b3c3d4bd60d040
apietrobon@ona.io <apietrobon@ona.io>
2022-07-19 15:05:47 +0000

    union_core_questions

--- macros/union_core_questions.sql
+++ macros/union_core_questions.sql
@@ -1,19 +1,25 @@
+{% set query %}
+select * from {{ref('stg_core_questions_land_survey')}} 
+{% endset %}
+
+{% set results = run_query(query) -%}
 
 -- create list of forms
-{% set forms = dbt_utils.get_column_values(table=ref('stg_core_questions_land_survey'), column='form_id') %}
+{% if execute %}
+    {% set forms = (results.select(['form_id']).columns[0].values()) | unique | list %}
+{% else %}
+    {% set forms = [] %}
+{% endif %}
 
 -- create list of fields
 {% set corefields = dbt_utils.get_column_values(table=ref('stg_core_questions_master'), column='name', where='is_land_survey=TRUE') %}
 
-
 {% for form in forms %}
-{% set schemaname = dbt_utils.get_column_values(table=ref('stg_core_questions_land_survey'), column='schemaname', where='is_land_survey=TRUE') %}
-{% set tablename = dbt_utils.get_column_values(table=ref('stg_core_questions_master'), column='name', where='is_land_survey=TRUE') %}
+
 
 -- for each form, loop through all the core fields, select the field with the appropriate name if present
     select 
     {{form}} as form_id, 
-
     {%- set formfields_query -%}
     select question_name, core_question_name
     from {{ref('stg_core_questions_land_survey')}}
@@ -30,22 +36,25 @@
         {{question_names[indexvalue]}}
         {%- else %}
         NULL 
-        {% endif -%} 
+        {% endif %} 
         as {{core_field}}
         {%- if not loop.last -%}
         ,
         {%- endif -%}
     {% endfor %}
 --find the appropriate table to join based on information in 'stg_core_questions_land_survey'
-from 
-
+    from 
+{% if execute -%}
+    {%- set indexvalue = (results.select(['form_id']).columns[0].values()  | list).index(form) -%}
+    {%- set schemaname = (results.select(['schemaname']).columns[0].values()  | list)[indexvalue] -%}
+    {%- set tablename = (results.select(['tablename']).columns[0].values()  | list)[indexvalue] -%}
+    {{schemaname}}.{{tablename}}
+{%- else -%}
+    {% set forms = [] %}
+    {% set schemaname = [] %}
+    {% set tablename = [] %}
+{%- endif -%}
 {% if not loop.last %}
     union all
-{% endif %}
-
-{% endfor %}
-
--- for each form: 
-    -- for each field in field list
-    -- select field if exists, select NULL as field
-    -- if not loop.last union all
+{%- endif -%}
+{%- endfor -%}
