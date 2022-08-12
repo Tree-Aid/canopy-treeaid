3961d7b45cf61b45ae306f8b60e364373df50602
jmesi@ona.io <jmesi@ona.io>
2022-08-12 10:16:38 +0000

    Update land sql

--- models/marts/land_surveys.sql
+++ models/marts/land_surveys.sql
@@ -1,2 +1,24 @@
-
+with land_survey as
+(
 {{survey_type_table('Land survey')}}
+)
+select 
+        form_name,
+        country,
+        project_code,
+        form_id,
+        submission_id,
+        initcap(replace(region,'_',' ')) as region,
+        initcap(replace(province,'_',' ')) as province,
+        initcap(replace(commune,'_',' ')) as commune,
+        date_assessment,
+        extract('Year' from date_assessment::date) as assessment_year,
+        gps_shape_area as gps_shape_area_m2,
+        {{area_in_hectares('gps_shape_area::FLOAT')}} as gps_shape_area_ha,
+        test,
+        geoshape,
+        initcap(replace(regexp_replace(nrm_category,'[^a-zA-Z,,_]','','g'),'_',' ')) as nrm_category,
+        initcap(replace(regexp_replace(soil_water_cons,'[^a-zA-Z,,_]','','g'),'_',' ')) as soil_water_cons,
+        initcap(replace(regexp_replace(gully_methods,'[^a-zA-Z,,_]','','g'),'_',' ')) as gully_methods
+    from land_survey
+    where test <> 'y'
--- models/marts/land_surveys_planted.sql
+++ models/marts/land_surveys_planted.sql
@@ -1,1 +1,23 @@
+with data as
+(
 {{survey_type_table('Land survey', repeat='planted')}} 
+)
+select
+d.form_name,
+d.country,
+d.project_code,
+d.form_id,
+d.id,
+d.submission_id,
+initcap(replace(ls.region,'_',' ')) as region,
+initcap(replace(ls.province,'_',' ')) as province,
+initcap(replace(ls.commune,'_',' ')) as commune,
+ls.date_assessment,
+extract('Year' from ls.date_assessment::date) as assessment_year,
+d.species,
+d.number_planted,
+d.number_regenerated
+from data d
+left join {{ ref('land_surveys') }} ls on d.submission_id=ls.submission_id
+where ls.test <>'y'
+
