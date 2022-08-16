af934b947706693ea84e43b31f0cfa3107b270c8
apietrobon@ona.io <apietrobon@ona.io>
2022-08-16 08:13:34 +0000

    rhomis indicators update

--- models/marts/land_surveys_planted.sql
+++ models/marts/land_surveys_planted.sql
@@ -9,15 +9,14 @@ d.project_code,
 d.form_id,
 d.id,
 d.submission_id,
-initcap(replace(ls.region,'_',' ')) as region,
-initcap(replace(ls.province,'_',' ')) as province,
-initcap(replace(ls.commune,'_',' ')) as commune,
+ls.region,
+ls.province,
+ls.commune,
 ls.date_assessment,
 extract('Year' from ls.date_assessment::date) as assessment_year,
 d.species,
 d.number_planted,
 d.number_regenerated
 from data d
-left join {{ ref('land_surveys') }} ls on d.submission_id=ls.submission_id
-where ls.test <>'y'
+inner join {{ ref('land_surveys') }} ls on d.submission_id=ls.submission_id
 
