cbd815e0f03917cdcd908aa83a4a2c6258f1bc8b
jmesi@ona.io <jmesi@ona.io>
2022-08-17 15:49:58 +0000

    enterprise survey update

--- models/marts/enterprise_survey_derivative.sql
+++ models/marts/enterprise_survey_derivative.sql
@@ -1,5 +1,2 @@
-{# Need to update this when there are more surveys run 
-{{survey_type_table('Enterprise survey', repeat='derivative')}} 
-#} 
 
-select 1 as number
+{{survey_type_table('Enterprise survey', repeat='derivative')}} 
