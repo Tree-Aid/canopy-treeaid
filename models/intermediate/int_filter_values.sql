4a133c5df89250d677fd07863af414b8c245865c
jmesi@ona.io <jmesi@ona.io>
2022-08-23 08:55:48 +0000

    Updating filter values

--- models/intermediate/int_filter_values.sql
+++ models/intermediate/int_filter_values.sql
@@ -5,7 +5,13 @@ select distinct country, project_code, assessment_year from {{ ref('enterprise_s
 union
 select distinct country, project_code, assessment_year from {{ ref('land_surveys') }}
 union
-select distinct country,project_code, extract('Year' from date_assessment::date) as assessment_year from {{ ref('land_surveys') }}
+select distinct country,project_code, extract('Year' from date_assessment::date) as assessment_year from {{ ref('rhomis_surveys') }}
+union
+select distinct country,project as project_code, year::DOUBLE PRECISION as assessment_year from {{ source('onadata', 'beneficiary_list') }}
 )
-select country,project_code,assessment_year from filter_values
+select 
+case when project_code like 'B%' then 'Burkina Faso' else country end as country,
+project_code,
+assessment_year 
+from filter_values
 where assessment_year is not null
