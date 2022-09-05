66065670367e4f2d95ebb3b24d0fba1aa0696cb2
jmesi@ona.io <jmesi@ona.io>
2022-09-05 07:29:44 +0000

    Updating Burkina Faso in the beneficiary list

--- models/intermediate/int_filter_values.sql
+++ models/intermediate/int_filter_values.sql
@@ -7,10 +7,10 @@ select distinct country, project_code, assessment_year from {{ ref('land_surveys
 union
 select distinct country,project_code, extract('Year' from date_assessment::date) as assessment_year from {{ ref('rhomis_surveys') }}
 union
-select distinct country,project as project_code, year::DOUBLE PRECISION as assessment_year from {{ source('onadata', 'beneficiary_list') }}
+select distinct case when project like 'B%' then 'Burkina Faso' else country end as country,project as project_code, year::DOUBLE PRECISION as assessment_year from {{ source('onadata', 'beneficiary_list') }}
 )
 select 
-case when project_code like 'B%' then 'Burkina Faso' else country end as country,
+country,
 project_code,
 assessment_year 
 from filter_values
