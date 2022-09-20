31e6fb61b97a313c8271032b5f39b838b1c26a80
Jane Mesi <jmesi@ona.io>
2022-09-20 13:04:54 +0000

    Adding land survey category 2

--- models/marts/land_surveys_planted.sql
+++ models/marts/land_surveys_planted.sql
@@ -16,7 +16,8 @@ ls.date_assessment,
 extract('Year' from ls.date_assessment::date) as assessment_year,
 d.species,
 d.number_planted,
-d.number_regenerated
+d.number_regenerated,
+ls.type_2
 from data d
 inner join {{ ref('land_surveys') }} ls on d.submission_id=ls.submission_id
 where ls.test not in ('y', 'Y','yes','Yes')
